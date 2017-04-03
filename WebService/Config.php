98772bd21ff48ce403e119cc4c1f40b7245b9e59
Thanggun99 <thanggun99@gmail.com>
2017-04-03 07:49:51 +0700

    cc

--- WebService/Config.php
+++
@@ -1,8 +0,0 @@
-    <?php
-    
-        define('DB_USERNAME', 'root');
-        define('DB_PASSWORD', '');
-        define('DB_HOST', 'localhost');
-        define('DB_NAME', 'db_quan_ly_nha_hang');
-        
-    ?>
